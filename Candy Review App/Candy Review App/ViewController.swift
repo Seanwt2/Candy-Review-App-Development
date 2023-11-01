7fe0224ee48ad2aa7272e65028f8e5086d1bb545
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-11-01 00:15:03 -1000

    Initial

---
+++ Candy Review App/Candy Review App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Candy Review App
+//
+//  Created by Sean Taylor on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
